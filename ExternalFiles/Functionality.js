cd7860bb3d1290919a4d221c80f69f3a3f841fbc
William Gavin Lacalle <digitalbros7@gmail.com>
2022-02-23 05:38:04 -0600

    Updates webpage navigation and disables scrolling on popup

--- ExternalFiles/Functionality.js
+++ ExternalFiles/Functionality.js
@@ -5,16 +5,25 @@ function switchPage(evt, pageName) {
         location.replace(baseURL + pageName + ".html");
     }
     else {
-        location.replace(baseURL + "WebsitePages/" + pageName + ".html");
+        window.location.replace(baseURL + "WebsitePages/" + pageName + ".html");
     }
 }
 
 function OpenPopup(overlayNumber) {
     let element = document.getElementById('overlay' + overlayNumber)
     element.style.display = 'block'
+
+    TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+    LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
+
+    window.onscroll = function() {
+        window.scrollTo(LeftScroll, TopScroll);
+    };
 }
 
 function ClosePopup(overlayNumber) {
     let element = document.getElementById('overlay' + overlayNumber)
     element.style.display = 'none'
+
+    window.onscroll = function() {};
 }
